4b816dab6e853856a9ad2716ff831fb150d4f400
Valerii20091995 <regaska0384@mail.ru>
2025-01-28 14:12:32 +0900

    создает файл index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,2 @@
 <?php
-$conn = pg_connect("host=db dbname=postgres user=valerii password=qwerty");
-if ($conn) {
-    echo "Connected to PostgreSQL successfully!";
-} else {
-    echo "Connection failed.";
-}
+echo "Hello World!";
